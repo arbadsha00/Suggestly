c92a6a392e7deb6db8b0d4c7a9439d0af413bfaf
arbadsha00 <arbadsha00@gmail.com>
2024-12-23 17:04:36 +0600

    rec for me added

--- src/pages/MyRecommendations.jsx
+++ src/pages/MyRecommendations.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
 
 const MyRecommendations = () => {
-    return (
-        <div>
-            MyRecommendations
-          
-        </div>
-    );
+    const [data, setData] = useState();
+    return <div className="container mx-auto p-4">
+      <h1 className="text-center text-primary text-4xl font-bold">
+      Explore Your Best Options
+      </h1>
+      <p className="md:w-3/4 mx-auto text-center my-2">
+        Choose the plan that best fits your needs. Upgrade anytime!
+      </p>
+  </div>;
 };
 
 export default MyRecommendations;
--- src/pages/Recommendations.jsx
+++ src/pages/Recommendations.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import AuthContext from "../provider/AuthContext";
+import { format } from "date-fns";
 
 const Recommendations = () => {
-    return (
-        <div>
-            Recommendations
-            
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const axiosSecure = useAxiosSecure();
+
+  useEffect(() => {
+    axiosSecure.get(`recommendations/user/${user?.email}`).then((res) => {
+      setData(res.data);
+      setLoading(false);
+    });
+  }, [user?.email, axiosSecure, data]);
+
+  return (
+    <div className="container mx-auto p-4 my-10">
+      <h1 className="text-center text-primary text-4xl font-bold">
+        Explore Your Best Options
+      </h1>
+      <p className="md:w-3/4 mx-auto text-center my-2">
+        Discover Products That Match Your Preferences
+      </p>
+      <div className="overflow-x-auto my-6 max-w-4xl mx-auto">
+        <table className="table text-xs md:text-base  table-zebra ">
+          <thead className="bg-primary text-white text-base rounded-xl">
+            <tr>
+              <th></th>
+              <th><span className="hidden md:inline">Recommended</span> Products</th>
+              <th><span  className="hidden md:inline">Recommended</span> Brands</th>
+              <th>Query <span  className="hidden md:inline ">Products</span> </th>
+              <th>Date <span  className="hidden md:inline">Added</span> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, idx) => (
+              <tr key={item._id}>
+                <th>{idx + 1}</th>
+                    <td>{item.recProduct}</td>
+                    <td>{item.recBrand}</td>
+                <td>{item.product}</td>
+                <td>{format(item.recDate, "d-M-yy")}</td>
+              
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Recommendations;
